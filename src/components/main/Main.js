4daf2d2244a50d45089f9c9a44fa4c9c5214bd1f
salohiddinov <yuldashevmirjalol40@gmail.com>
2024-03-27 13:52:33 +0300

    muhammadal1

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -48,25 +48,21 @@ function Main() {
                         <div className="title">
                             <h2>I will wake up at 8 in the morning</h2>
                         </div>
-                        <button className='toggle__btn'> <RiDeleteBin6Line className='btn__svg' /> </button>
                     </div>
                     <div className='togle__const'>
                         <div className="title">
                             <h2>I will practice html for 1 hour</h2>
                         </div>
-                        <button className='toggle__btn'> <RiDeleteBin6Line className='btn__svg' /> </button>
                     </div>
                     <div className='togle__const'>
                         <div className="title">
                             <h2>I will give time for 2 hour css</h2>
                         </div>
-                        <button className='toggle__btn'> <RiDeleteBin6Line className='btn__svg' /> </button>
                     </div>
                     <div className='togle__const'>
                         <div className="title">
                             <h2>then I will have breakfast</h2>
                         </div>
-                        <button className='toggle__btn'> <RiDeleteBin6Line className='btn__svg' /> </button>
                     </div>
                 </div> :
                     <></>
